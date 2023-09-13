f2b0924521be62d150286aabe45ca3bff4ca128a
willy950205 <willy950205@gmail.com>
2023-09-13 18:52:44 +0900

    project build

---
+++ src/main/java/org/side/med/MedAlchemistApplication.java
@@ -0,0 +1,13 @@
+package org.side.med;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedAlchemistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedAlchemistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/side/med/MedAlchemistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.side.med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedAlchemistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
