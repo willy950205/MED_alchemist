c0fd3ad5cd331f9e07f4d320fb90e4f65ac671de
mchooooo <mchooooo@naver.com>
2023-10-31 18:05:31 +0900

    API 호출 구현중 ㅋㅋ

---
+++ src/main/java/org/side/med/dto/DrugRequestDto.java
@@ -0,0 +1,12 @@
+package org.side.med.dto;
+
+import lombok.Data;
+
+@Data
+public class DrugRequestDto {
+    private String pageNo;
+    private String numOfRows;
+    private String entpName;
+    private String itemName;
+
+}
